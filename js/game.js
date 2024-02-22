4909e5749641fccd36d7b49c1175ee98b41a101a
Simon <semenpetrakov@gmail.com>
2024-02-22 22:07:11 +0300

    js

---
+++ js/game.js
@@ -0,0 +1,9 @@
+const plusButton = document.getElementById('plus-button');
+const minusButton = document.getElementById('minus-button');
+const divideButton = document.getElementById('divide-button');
+const multiplyButton = document.getElementById('multiply-button');
+
+const resetButton = document.getElementById('restart-button');
+
+let zums = [1, 2, 3, 4];
+
